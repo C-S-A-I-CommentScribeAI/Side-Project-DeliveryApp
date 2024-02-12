e43f492fc2d9a4ce3bda4762d5ec789118bc22d9
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-02-12 21:07:44 +0900

    feat : drawer(사이드바) 메뉴 구현
    UserAccountsDrawerHeader 함수를 사용해 프로필 구현, align 위젯을 사용해 리스트 버튼 하단에 고정

--- frontend/lib/home_screen.dart
+++ frontend/lib/home_screen.dart
@@ -14,33 +14,86 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CSAI 사장님'),
         elevation: 10,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.green[800],
         foregroundColor: Colors.white,
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              // 로그인 페이지로 이동
-              if (!mounted) return;
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginPage(),
-                ),
-              );
-            },
+            onPressed: () async {},
             icon: const Icon(
-              Icons.logout,
+              Icons.account_circle,
             ),
           ),
         ],
+      ),
+      // drawer 메뉴(사이드바 메뉴)
+      // Drawer() 위젯의 경우 기본적으로 Scaffold() 위젯 내에 존재
+      drawer: Drawer(
+        // 프로필 헤더 밑에 스택처럼 쌓이는 ListTile들과 하단에 고정시키는 ListTile들을 다른 위젯으로 보기 위해 Column으로 설정
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  // 위쪽에 계정(프로필, 이름, 이메일) 표시
+                  UserAccountsDrawerHeader(
+                    // 현재 계정 이미지
+                    currentAccountPicture: const CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/짱구.png'),
+                      backgroundColor: Colors.white,
+                    ),
+                    accountName: const Text('민택기'),
+                    accountEmail: const Text('tk@naver.com'),
+                    decoration: BoxDecoration(
+                      color: Colors.green[800],
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(40.0),
+                        bottomRight: Radius.circular(40.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Align : 부모의 위젯 안에서 자식 위젯을 배치시킬 수 있는 위젯
+            // 하단에 고정된 상태로 유지
+            Align(
+                alignment: FractionalOffset.bottomCenter, // 중앙하단 배치
+                child: Column(
+                  children: [
+                    const Divider(), // 경계선
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text('Settings'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.logout),
+                      title: const Text('Logout'),
+                      onTap: () async {
+                        await _auth.signOut();
+                        // 로그인 페이지로 이동
+                        if (!mounted) return;
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                )),
+          ],
+        ),
       ),
       body: Center(
         child: Column(
@@ -86,7 +139,7 @@ class _HomePageState extends State<HomePage> {
                           minimumSize: const Size(160, 150),
                         ),
                         child: const Text(
-                          '고객관리',
+                          '가게관리',
                           style: TextStyle(
                             color: Colors.black,
                             fontSize: 20,
